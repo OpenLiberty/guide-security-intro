e6e191dab52ac13d9e9aad8381605017ed430e84
Justine Chen <justine.chen@ibm.com>
2018-07-05 17:31:54 -0400

    CustomFormAuth working for application, basic auth tests have not been updated to work for new URLS

--- finish/src/main/java/io/openliberty/guides/frontend/LoginBean.java
+++ finish/src/main/java/io/openliberty/guides/frontend/LoginBean.java
@@ -19,25 +19,43 @@ import javax.servlet.http.*;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import javax.enterprise.context.RequestScoped;
+import javax.faces.annotation.FacesConfig;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.security.enterprise.AuthenticationStatus;
+import javax.security.enterprise.SecurityContext;
+import javax.security.enterprise.credential.Credential;
+import javax.security.enterprise.credential.Password;
+import javax.security.enterprise.credential.UsernamePasswordCredential;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import static javax.faces.annotation.FacesConfig.Version.JSF_2_3;
+import static javax.faces.application.FacesMessage.SEVERITY_ERROR;
+import static javax.security.enterprise.AuthenticationStatus.SEND_CONTINUE;
+import static javax.security.enterprise.AuthenticationStatus.SEND_FAILURE;
+import static javax.security.enterprise.authentication.mechanism.http.AuthenticationParameters.withParams;
 
-import javax.security.enterprise.authentication.mechanism.http.LoginToContinue;
-import javax.security.enterprise.authentication.mechanism.http.FormAuthenticationMechanismDefinition;
-import javax.security.enterprise.authentication.mechanism.http.BasicAuthenticationMechanismDefinition;
+
 
 
 import io.openliberty.guides.frontend.SessionUtils;
 
 @ManagedBean
 @ViewScoped
-@BasicAuthenticationMechanismDefinition(
-    realmName = "myRealm")
-@FormAuthenticationMechanismDefinition(
-  loginToContinue = @LoginToContinue(
-    loginPage = "/index.jsf",
-    errorPage = "/loginerror.jsf",
-    useForwardToLogin = true)
-  )
+
+@FacesConfig(version = JSF_2_3)
+@Named
+@RequestScoped
   public class LoginBean {
+
+    @Inject 
+    private SecurityContext securityContext;
+
+    @Inject
+    private FacesContext facesContext;
 
     private String username;
     private String password;
@@ -58,19 +76,69 @@ import io.openliberty.guides.frontend.SessionUtils;
         return password;
     }
 
-    public String doLogin() throws Exception {
-        HttpServletRequest request = SessionUtils.getRequest();
+        private static HttpServletResponse getResponseFrom(FacesContext context) {
+        return (HttpServletResponse) context
+            .getExternalContext()
+            .getResponse();
+    }
+    
+    private static HttpServletRequest getRequestFrom(FacesContext context) {
+        return (HttpServletRequest) context
+            .getExternalContext()
+            .getRequest();
+    }
+    
+    private static void addError(FacesContext context, String message) {
+        context
+            .addMessage(
+                null, 
+                new FacesMessage(SEVERITY_ERROR, message, null));
+    }
 
-        // do login
-        try {
-            request.logout();
-            request.login(this.username, this.password);
-        } catch (ServletException e) {
-            System.out.println("Login failed.");
-            return "error.jsf";
+    public void doLogin() {
+        Credential credential = new UsernamePasswordCredential(this.username, this.password);
+
+        AuthenticationStatus status = securityContext.authenticate(
+            getRequestFrom(facesContext),
+            getResponseFrom(facesContext),
+            withParams().credential(credential));
+        if (status.equals(SEND_CONTINUE)) {
+    
+            facesContext.responseComplete();
+            //         try{
+            //     facesContext.getCurrentInstance().getExternalContext().redirect("index.jsf");
+            //     return;
+            // } catch (IOException e){
+            //  //error
+            // }
+
+            
+        } else if (status.equals(SEND_FAILURE)) {
+            addError(facesContext, "Authentication failed");
         }
 
-        return "index.jsf";
+
+        // HttpServletRequest request = SessionUtils.getRequest();
+        // try {
+        //     request.logout();
+        //     request.login(this.username, this.password);
+        // } catch (ServletException e) {
+        //     return "no";
+        // }
+        // return "yes";
+        
+        // do login
+        // try {
+        //     request.logout();
+        //     request.login(this.username, this.password);
+            
+        // } catch (ServletException e) {
+        //     System.out.println("Login failed.");
+
+        //     return "error.jsf";
+        // }
+
+        // return "index.jsf";
     }
 
 }
