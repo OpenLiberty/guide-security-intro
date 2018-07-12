7437d6bef7b497a3519fca2e173297e05ad47675
Justine Chen <justine.chen@ibm.com>
2018-07-12 11:14:31 -0400

    Updated app

--- finish/src/test/java/it/io/openliberty/guides/rest/SecurityTest.java
+++ finish/src/test/java/it/io/openliberty/guides/rest/SecurityTest.java
@@ -33,6 +33,14 @@ import javax.ws.rs.core.Response;
 import javax.json.JsonArray;
 import javax.ws.rs.core.Form;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+
+
 
 public class SecurityTest {
 	private static String httpPort;
@@ -71,6 +79,7 @@ public class SecurityTest {
 
 	@Test
 	public void testSuite(){
+		// this.testForm();
 		// this.testCorrectAdmin();
 		// this.testCorrectUser();
 		// this.testIncorrectUser();
@@ -93,17 +102,20 @@ public class SecurityTest {
 		assertEquals(expectedResponseStatus, actualResponseStatus);
 	}
 
+	// public void testForm(){
+	// 	int e = 0;
+	// 	int a = testLogInHttpClient("bob:bobpwd");
+	// 	assertEquals(e,a);
+	// }
 
+		//Helper function - to be removed
+	 //Reads the page given the response
 		public void printPage(Response response){
-
-					// System.out.println("Response status of page I am printing:");
 					System.out.println(response.getStatus());
 				 BufferedReader br = new BufferedReader(new InputStreamReader((InputStream) response.getEntity()));
 	    List<String> result = new ArrayList<String>();
-	    // System.out.println("REACHED before the try");
 	    try {
 	    	String input;
-	    	// System.out.println("inside try");
 	    	while ((input = br.readLine()) != null){
 	    		System.out.println(input);
 
@@ -115,6 +127,8 @@ public class SecurityTest {
 	    	fail();
 	    }
 		}
+
+
 		public int logIn(String usernameAndPassword){
 			String authHeader = "Basic "
 	        + java.util.Base64.getEncoder()
@@ -134,5 +148,31 @@ public class SecurityTest {
     return loginResponseValue;
 
 		}
+
+		// public int testLogInHttpClient(String usernameAndPassword){
+		// 	String authHeader = "Form "
+  //     + java.util.Base64.getEncoder()
+  //                       .encodeToString(usernameAndPassword.getBytes());
+
+  //  HttpClient client = new DefaultHttpClient();
+  //  HttpGet request = new HttpGet("http://localhost:9080/LibertyProject/primary.jsf");
+
+  //  request.addHeader(authHeader);
+
+  //  HttpResponse response = client.execute(request);
+
+  //  BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+
+  //  StringBuffer result = new StringBuffer();
+  //  String line = "";
+  //  while ((line = rd.readLine()) != null) {
+  //  	result.append(line);
+  //  }
+
+  //  System.out.println(result.toString());
+
+  //  return 0;
+
+		// }
 
 }
