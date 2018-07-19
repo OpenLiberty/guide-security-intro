d862c5e992efc177e0cb61daf703c75d24b16cc2
Justine Chen <justine.chen@ibm.com>
2018-07-19 17:15:04 -0400

    Tests for basic auth servlet app completed

--- finish/src/test/java/io/openliberty/guides/hello/it/SecurityTest.java
+++ finish/src/test/java/io/openliberty/guides/hello/it/SecurityTest.java
@@ -13,14 +13,32 @@
 package io.openliberty.guides.hello.it;
 
 import static org.junit.Assert.*;
+import java.io.*;
+import java.util.*;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.commons.httpclient.methods.GetMethod;
+
 
 public class SecurityTest {
+    private Client client;
+
     private static String URL;
+    private final String ADMIN_NAME = "bob:bobpwd";
+    private final String ADMIN_SERVLET = "adminonly";
+
+    private final String USER_NAME = "alice:alicepwd";
+    private final String USER_SERVLET = "servlet";
+
+    private final String INCORRECT_NAME = "carl:carlpwd";
+ 
 
  
     @BeforeClass
@@ -29,28 +47,63 @@ public class SecurityTest {
         // tag::URL[]
         String port = System.getProperty("liberty.test.port");
         String war = System.getProperty("war.name");
-        URL = "http://localhost:" + port + "/" + war + "/" + "servlet";
+        URL = "http://localhost:" + port + "/" + war + "/";
         // end::URL[]
         
     }
 
-    @Test
-    public void testServlet() throws Exception {
-        HttpClient client = new HttpClient();
-
-        GetMethod method = new GetMethod(URL);
-        // tag::clicklink[]
-        try {
-            int statusCode = client.executeMethod(method);
-
-            assertEquals("HTTP GET failed", HttpStatus.SC_OK, statusCode);
-
-            String response = method.getResponseBodyAsString(1000);
-
-            assertTrue("Unexpected response body", response.contains("Hello! How are you today?"));
-        } finally {
-            method.releaseConnection();
-        }
-        // end::clicklink[]
+    @Before
+    public void setup(){
+        client = ClientBuilder.newClient();
     }
+
+    @After 
+    public void teardown() {
+        client.close();
+    }
+
+    @Test
+    public void testSuite(){
+        this.testCorrectAdmin();
+        this.testCorrectUser();
+        this.testIncorrectUser();
+    }
+
+    public void testCorrectAdmin(){
+        int expectedResponseStatus = 200;
+        int actualResponseStatus = logIn(ADMIN_NAME, ADMIN_SERVLET);
+        assertEquals(expectedResponseStatus, actualResponseStatus);
+    }
+
+    public void testCorrectUser(){
+        int expectedResponseStatus = 403;
+        int actualResponseStatus = logIn(USER_NAME, ADMIN_SERVLET);
+        assertEquals(expectedResponseStatus, actualResponseStatus);
+    }
+
+    public void testIncorrectUser(){
+        int expectedResponseStatus = 401;
+        int actualResponseStatus = logIn(INCORRECT_NAME, USER_SERVLET);
+        assertEquals(expectedResponseStatus, actualResponseStatus);
+    }
+
+
+    public int logIn(String usernameAndPassword, String servlet){
+        String authHeader = "Basic "
+            + java.util.Base64.getEncoder()
+                              .encodeToString(usernameAndPassword.getBytes());
+
+        Response response = client.target("http://localhost:9080/ServletSample/" + servlet)
+                                         .request(MediaType.APPLICATION_JSON)
+                                         .header("Authorization",
+                                             authHeader)
+                                         .get();
+
+        int loginResponseValue = response.getStatus();
+        response.close();
+
+        return loginResponseValue;
+
+    }
+
 }
