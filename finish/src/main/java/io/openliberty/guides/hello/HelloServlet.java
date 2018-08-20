1735c242b0c2eb5465de5f6dfd417b7f404863d5
Junyi Sun <junyisun@junyis-mbp.war.can.ibm.com>
2018-08-20 15:55:13 -0400

    update

--- finish/src/main/java/io/openliberty/guides/hello/HelloServlet.java
+++ finish/src/main/java/io/openliberty/guides/hello/HelloServlet.java
@@ -44,7 +44,7 @@ public class HelloServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().append("Hello! How are you today?\n");
+        response.getWriter().append("Hello User! How are you today?\n");
     }
 
     /**
