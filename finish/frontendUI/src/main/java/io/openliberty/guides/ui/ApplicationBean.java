f9463c04006f6cd86ff6a4d6d066976327b88d2a
Junyi Sun <junyisun@junyis-mbp.war.can.ibm.com>
2018-08-21 16:54:41 -0400

    update

---
+++ finish/frontendUI/src/main/java/io/openliberty/guides/ui/ApplicationBean.java
@@ -0,0 +1,94 @@
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2018 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+// end::copyright[]
+// tag::jwt[]
+package io.openliberty.guides.ui;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import io.openliberty.guides.ui.models.SystemModel;
+import io.openliberty.guides.ui.util.ServiceUtils;
+
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import javax.annotation.security.RolesAllowed;
+import javax.annotation.security.DeclareRoles;
+
+import  javax.servlet.annotation.ServletSecurity;
+import javax.servlet.annotation.HttpConstraint;
+
+
+import javax.security.enterprise.authentication.mechanism.http.FormAuthenticationMechanismDefinition;
+import javax.security.enterprise.authentication.mechanism.http.LoginToContinue;
+
+
+@ManagedBean
+@ViewScoped
+public class ApplicationBean {
+
+  private String hostname;
+
+
+  public String getOs() {
+
+    if (ServiceUtils.responseOk()) {
+      JsonObject properties = ServiceUtils.getProperties();
+      return properties.getString("os.name");
+    }
+    return "You are not authorized to access the system service.";
+  }
+
+  public String getInventorySize() {
+
+    if (ServiceUtils.invOk()) {
+      JsonObject properties = ServiceUtils.getInventory();
+      return String.valueOf(properties.getInt("total"));
+    }
+    return "You are not authorized to access the inventory service.";
+  }
+
+  public List<SystemModel> getInventoryList() {
+
+    if (ServiceUtils.invOk()) {
+      JsonArray systems = ServiceUtils.getInventory().getJsonArray("systems");
+      return systems.stream().map(s -> new SystemModel((JsonObject) s)).collect(Collectors.toList());
+    }
+
+    return Collections.emptyList();
+  }
+
+  public String getHostname() {
+    return hostname;
+  }
+
+  public void setHostname(String hostname) {
+    this.hostname = hostname;
+
+    if (ServiceUtils.invOk()) {
+      ServiceUtils.addSystem(hostname);
+    }
+  }
+
+
+}
+// end::jwt[]
