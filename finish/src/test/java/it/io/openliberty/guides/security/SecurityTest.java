96271810fdd9f79876c2bc5930b33e57b5c147f2
Justine Chen <justinechen619@gmail.com>
2018-09-24 10:16:43 -0400

    Created Start directory and updated README.adoc (#27)
    * Updated finish/start dirs from Gilbert's branch, started to modify README.adoc
    
    * Included paragraph about why encryption key file is stored in /config
    
    * Updated servlet names in README
    
    * Gilbert's updated Security Test with comments
    
    * Updated README
    
    * Read me updates
    
    * Updated README up to Encrypting..
    
    * Updated readme, not including the test description
    
    * Start dir
    
    * Updated README
    
    * Updated README
    
    * Updated test section in readme
    
    * Updated read me
    
    * Removed space in Home Servlet
    
    * Updated readme
    
    * Updated readme
    
    * Updated UserBean.java in start folder and README
    
    *  updated web.xml

---
+++ finish/src/test/java/it/io/openliberty/guides/security/SecurityTest.java
@@ -0,0 +1,169 @@
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2018 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+// end::copyright[]
+// tag::test[]
+package it.io.openliberty.guides.security;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.net.URI;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SecurityTest {
+
+  private static String urlHttp;
+  private static String urlHttps;
+
+  private void trustAll() throws Exception {
+          SSLContext sslContext = SSLContext.getInstance("SSL");
+          sslContext.init(
+            null, 
+            new TrustManager[] {
+              new X509TrustManager() {
+                  @Override
+                    public void checkClientTrusted(X509Certificate[] arg0, String arg1)
+                      throws CertificateException {}
+
+                  @Override
+                    public void checkServerTrusted(X509Certificate[] arg0, String arg1)
+                      throws CertificateException {}
+
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return null;
+                    }
+              }
+            },
+            new SecureRandom());
+          SSLContext.setDefault(sslContext);
+          HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+  }
+
+  @Before
+  public void setup() throws Exception {
+    urlHttp = "http://localhost:" + System.getProperty("liberty.test.port");
+    urlHttps = "https://localhost:" + System.getProperty("liberty.test.ssl.port");
+    trustAll();
+  }
+
+  @Test
+  public void testAuthenticationFail() throws Exception {
+    executeURL("/user", "bob", "wrongpassword", true, -1, "Don't care");
+    System.out.println("testAuthenticationFail passed!");
+  }
+
+  @Test
+  public void testAuthenticationSucceed() throws Exception {
+    executeURL("/user", "bob", "bobpwd", false, 
+      HttpServletResponse.SC_OK, "UserServlet");
+    System.out.println("testAuthenticationSucceed passed!");
+  }
+
+  @Test
+  public void testAuthorizationForAdmin() throws Exception {
+    executeURL("/admin", "bob", "bobpwd", false, 
+      HttpServletResponse.SC_OK, "AdminServlet");
+    System.out.println("testAuthorizationForAdmin passed!");
+  }
+
+  @Test
+  public void testAuthorizationForUser() throws Exception {
+    executeURL("/admin", "alice", "alicepwd", false, 
+      HttpServletResponse.SC_FORBIDDEN, "Error 403: AuthorizationFailed");
+    System.out.println("testAuthorizationForUser passed!");
+  }
+
+  private void executeURL(
+    String testUrl, String userid, String password,
+    boolean expectLoginFail, int expectedCode, String expectedContent) 
+    throws Exception {
+
+    // Use HttpClient to execute the testUrl by HTTP
+    URI url = new URI(urlHttp + testUrl);
+    HttpGet getMethod = new HttpGet(url);
+    HttpClientBuilder clientBuilder = HttpClientBuilder.create();
+    SSLContext sslContext = SSLContext.getDefault();
+    clientBuilder.setSSLContext(sslContext);
+    clientBuilder.setDefaultRequestConfig(
+      RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD).build());
+    HttpClient client = clientBuilder.build();
+    HttpResponse response = client.execute(getMethod);
+
+    // The response should return the login.html
+    String loginBody = EntityUtils.toString(response.getEntity(), "UTF-8");
+    assertTrue(
+      "The login.html was not redirected", 
+      loginBody.contains("j_security_check"));
+
+    // Use j_security_check to login
+    HttpPost postMethod = new HttpPost(urlHttps + "/j_security_check");
+    List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+    nvps.add(new BasicNameValuePair("j_username", userid ));
+    nvps.add(new BasicNameValuePair("j_password", password));
+    postMethod.setEntity(new UrlEncodedFormEntity(nvps, "UTF-8"));
+    response = client.execute(postMethod);
+    assertEquals(
+      "Expected " + HttpServletResponse.SC_FOUND + " status code for login",
+      HttpServletResponse.SC_FOUND, response.getStatusLine().getStatusCode());
+
+    // Return if expect the login should fail
+    if (expectLoginFail) {
+      String location = response.getFirstHeader("Location").getValue();
+      assertTrue(
+        "Error.html was not returned", 
+        location.contains("error.html"));
+      return;
+    }
+
+    // Use HttpClient to execute the testUrl by HTTPS
+    url = new URI(urlHttps + testUrl);
+    getMethod = new HttpGet(url);
+    response = client.execute(getMethod);
+    assertEquals(
+      "Expected " + expectedCode + " status code for login",
+      expectedCode, response.getStatusLine().getStatusCode());
+    
+    // Check the content of the response returned
+    String actual = EntityUtils.toString(response.getEntity(), "UTF-8");
+    assertTrue(
+      "The url " + testUrl + 
+      " did not return the expected content \"" + expectedContent + "\"" +
+      "The actual content was: \"" + actual + "\".", 
+      actual.contains(expectedContent));
+  }
+
+}
+// end::test[]
